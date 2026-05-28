02fbdd981af8ed8b815df3f3ab8607f4322effff
Prayush <prayushbogati0120@gmail.com>
2026-05-28 16:30:49 +0545

    managed some files for pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').Config} */
 const nextConfig = {
   output: 'export',
-  basePath: '/your-repo-name', // required for GitHub Pages
+  basePath: '/News-App-NextJS', // required for GitHub Pages
   images: {
     unoptimized: true, // GitHub Pages can't optimize images
   },
